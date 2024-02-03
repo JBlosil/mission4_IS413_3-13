574e9675d9db77a469159e6c87280de4b2ec3948
Jakob <owner@OWNERE36D>
2024-02-02 22:14:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using mission4;
+
+char[] board = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+int choice;
+// Call the function to display the Tic Tac Toe board
+Game.DisplayTicTacToeBoard(board);
+bool win = false;
+
+// Ask each player in turn for their choice and update the game board array
+for (int i = 0; i <= 8; i++)
+{
+    if (i % 2 == 0 && win == false)
+    {
+        Console.WriteLine("Player 1, where would you like to go?");
+        choice = int.Parse(Console.ReadLine()) - 1;
+        board[choice] = 'X';
+        Game.DisplayTicTacToeBoard(board);
+        if (Game.CheckWinner(board) == true)
+        {
+            win = true;
+        }
+        else if (i == 8 && Game.CheckWinner(board) == false)
+        {
+            win = true;
+            Console.WriteLine("It's a tie!");
+        }
+        else
+        {
+            win = false;
+        }
+    }
+    else if (i % 2 != 0 && win == false)
+    {
+        Console.WriteLine("Player 2, where would you like to go?");
+        choice = int.Parse(Console.ReadLine()) - 1;
+        board[choice] = 'O';
+        Game.DisplayTicTacToeBoard(board);
+        if (Game.CheckWinner(board) == true)
+        {
+            win = true;
+        }
+        else
+        {
+            win = false;
+        }
+    }
+}
---
+++ game.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission4
+{
+    internal class Game
+    {
+        public static void DisplayTicTacToeBoard(char[] board)
+        {
+            Console.WriteLine("Tic Tac Toe Board:");
+            for (int i = 0; i < board.Length; i += 3)
+            {
+                Console.WriteLine(" {0} | {1} | {2} ", board[i], board[i + 1], board[i + 2]);
+                if (i < board.Length - 3) // To prevent printing the separator after the last line
+                {
+                    Console.WriteLine("---+---+---");
+                }
+            }
+        }
+
+        // Contain a method that receives the game board array as input
+        // and returns if there is a winner and who it was
+        public static bool CheckWinner(char[] board)
+        {
+            // Check for a winner
+            if ((board[0] == board[1] && board[1] == board[2] && board[0] != ' ') ||
+                (board[3] == board[4] && board[4] == board[5] && board[3] != ' ') ||
+                (board[6] == board[7] && board[7] == board[8] && board[6] != ' ') ||
+                (board[0] == board[3] && board[3] == board[6] && board[0] != ' ') ||
+                (board[1] == board[4] && board[4] == board[7] && board[1] != ' ') ||
+                (board[2] == board[5] && board[5] == board[8] && board[2] != ' ') ||
+                (board[0] == board[4] && board[4] == board[8] && board[0] != ' ') ||
+                (board[2] == board[4] && board[4] == board[6] && board[2] != ' '))
+            {
+                if (board[0] == 'X' || board[3] == 'X' || board[6] == 'X' ||
+                    board[1] == 'X' || board[4] == 'X' || board[7] == 'X' ||
+                    board[2] == 'X' || board[5] == 'X' || board[8] == 'X')
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                }
+                else
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                }
+                return true;
+            }
+            if (board[0] != 'X' || board[1] != 'X' || board[2] != 'X' || board[3] != 'X' || board[4] != 'X' ||
+                board[5] != 'X' || board[6] != 'X' || board[7] != 'X' || board[8] != 'X' || board[1] != 'O' ||
+                board[2] != 'O' || board[3] != 'O' || board[4] != 'O' || board[5] != 'O' || board[6] != 'O' ||
+                board[7] != 'O' || board[8] != 'O' || board[0] != 'O')
+            {
+                return false;
+            }
+            Console.WriteLine("It's a tie!");
+            return true;
+        }
+    }
+}
